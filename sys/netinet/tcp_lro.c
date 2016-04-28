0f2b141c14485277b2f0890044137eae26b0dc07
Sepherosa Ziehau <sephe@dragonflybsd.org>
2016-04-28 13:14:40 +0800

    tcp/lro: Implement hash table for LRO entry lookup.

--- sys/netinet/tcp_lro.c
+++ sys/netinet/tcp_lro.c
@@ -70,17 +70,20 @@ static MALLOC_DEFINE(M_LRO, "LRO", "LRO control structures");
 static void	tcp_lro_rx_done(struct lro_ctrl *lc);
 
 static __inline void
-tcp_lro_active_insert(struct lro_ctrl *lc, struct lro_entry *le)
+tcp_lro_active_insert(struct lro_ctrl *lc, struct lro_head *bucket,
+    struct lro_entry *le)
 {
 
 	LIST_INSERT_HEAD(&lc->lro_active, le, next);
+	LIST_INSERT_HEAD(bucket, le, hash_next);
 }
 
 static __inline void
 tcp_lro_active_remove(struct lro_entry *le)
 {
 
-	LIST_REMOVE(le, next);
+	LIST_REMOVE(le, next);		/* active list */
+	LIST_REMOVE(le, hash_next);	/* hash bucket */
 }
 
 static __inline struct lro_entry *
@@ -114,7 +117,7 @@ tcp_lro_init_args(struct lro_ctrl *lc, struct ifnet *ifp,
 {
 	struct lro_entry *le;
 	size_t size;
-	unsigned i;
+	unsigned i, elements;
 
 	lc->lro_bad_csum = 0;
 	lc->lro_queued = 0;
@@ -128,6 +131,18 @@ tcp_lro_init_args(struct lro_ctrl *lc, struct ifnet *ifp,
 	lc->ifp = ifp;
 	LIST_INIT(&lc->lro_free);
 	LIST_INIT(&lc->lro_active);
+
+	/* create hash table to accelerate entry lookup */
+	if (lro_entries > lro_mbufs)
+		elements = lro_entries;
+	else
+		elements = lro_mbufs;
+	lc->lro_hash = phashinit_flags(elements, M_LRO, &lc->lro_hashsz,
+	    HASH_NOWAIT);
+	if (lc->lro_hash == NULL) {
+		memset(lc, 0, sizeof(*lc));
+		return (ENOMEM);
+	}
 
 	/* compute size to allocate */
 	size = (lro_mbufs * sizeof(struct mbuf *)) +
@@ -165,6 +180,13 @@ tcp_lro_free(struct lro_ctrl *lc)
 		tcp_lro_active_remove(le);
 		m_freem(le->m_head);
 	}
+
+	/* free hash table */
+	if (lc->lro_hash != NULL) {
+		free(lc->lro_hash, M_LRO);
+		lc->lro_hash = NULL;
+	}
+	lc->lro_hashsz = 0;
 
 	/* free mbuf array, if any */
 	for (x = 0; x != lc->lro_mbuf_count; x++)
@@ -533,6 +555,8 @@ tcp_lro_rx(struct lro_ctrl *lc, struct mbuf *m, uint32_t csum)
 	tcp_seq seq;
 	int error, ip_len, l;
 	uint16_t eh_type, tcp_data_len;
+	struct lro_head *bucket;
+	uint32_t hash;
 
 	/* We expect a contiguous header [eh, ip, tcp]. */
 
@@ -625,8 +649,29 @@ tcp_lro_rx(struct lro_ctrl *lc, struct mbuf *m, uint32_t csum)
 
 	seq = ntohl(th->th_seq);
 
+	switch (eh_type) {
+#ifdef INET
+	case ETHERTYPE_IP:
+		hash = ip4->ip_src.s_addr + ip4->ip_dst.s_addr;
+		break;
+#endif
+#ifdef INET6
+	case ETHERTYPE_IPV6:
+		hash = ip6->ip6_src.s6_addr32[0] + ip6->ip6_dst.s6_addr32[0];
+		hash += ip6->ip6_src.s6_addr32[1] + ip6->ip6_dst.s6_addr32[1];
+		hash += ip6->ip6_src.s6_addr32[2] + ip6->ip6_dst.s6_addr32[2];
+		hash += ip6->ip6_src.s6_addr32[3] + ip6->ip6_dst.s6_addr32[3];
+		break;
+#endif
+	default:
+		hash = 0;
+		break;
+	}
+	hash += th->th_sport + th->th_dport;
+	bucket = &lc->lro_hash[hash % lc->lro_hashsz];
+
 	/* Try to find a matching previous segment. */
-	LIST_FOREACH(le, &lc->lro_active, next) {
+	LIST_FOREACH(le, bucket, hash_next) {
 		if (le->eh_type != eh_type)
 			continue;
 		if (le->source_port != th->th_sport ||
@@ -733,7 +778,7 @@ tcp_lro_rx(struct lro_ctrl *lc, struct mbuf *m, uint32_t csum)
 		return (TCP_LRO_NO_ENTRIES);
 
 	/* Start a new segment chain. */
-	tcp_lro_active_insert(lc, le);
+	tcp_lro_active_insert(lc, bucket, le);
 	getmicrotime(&le->mtime);
 
 	/* Start filling in details. */
