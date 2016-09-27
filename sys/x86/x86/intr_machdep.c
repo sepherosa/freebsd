ad98b35bd2357459a53743079754b64bc0af653c
Sepherosa Ziehau <sephe@dragonflybsd.org>
2016-09-27 14:12:26 +0800

    Revert "x86: Don't shuffle interrupts target CPUs on Hyper-V."
    This reverts commit 1ba2f2b5aa5778c1b775b70432a35692edf1d5cf.

--- sys/x86/x86/intr_machdep.c
+++ sys/x86/x86/intr_machdep.c
@@ -564,10 +564,6 @@ intr_shuffle_irqs(void *arg __unused)
 	if (mp_ncpus == 1)
 		return;
 
-	/* Does not work properly on Hyper-V. */
-	if (vm_guest == VM_GUEST_HV)
-		return;
-
 	/* Round-robin assign a CPU to each enabled source. */
 	sx_xlock(&intrsrc_lock);
 	assign_cpu = 1;
